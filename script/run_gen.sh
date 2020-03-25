3c1decda78e62608272b6601eadf780cfcf8e8f6
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-03-25 15:55:02 +0900

    Update run_gen.sh

--- script/run_gen.sh
+++ script/run_gen.sh
@@ -5,10 +5,12 @@ TDIR="data_setup"
 SDIR="script"
 IDIR="input_data"
 ODIR="gen_data"
+MDIR="covid19"
 
 SPATH=${WDIR}/${TDIR}/${SDIR}
 IPATH=${WDIR}/${TDIR}/${IDIR}
 OPATH=${WDIR}/${TDIR}/${ODIR}
+MPATH=${WDIR}/${MDIR}
 
 # exec
 
@@ -23,3 +25,17 @@ echo "${SPATH}/merge_json.py"
 
 echo "sed 's/\\\\/\\/g' ${OPATH}/data_new.json > ${OPATH}/data.json"
       sed 's/\\\\/\\/g' ${OPATH}/data_new.json > ${OPATH}/data.json
+
+# to master
+
+echo "cp ${OPATH}/data.json ${MPATH}/data/data.json"
+      cp ${OPATH}/data.json ${MPATH}/data/data.json
+
+echo "cd ${MPATH}"
+      cd ${MPATH}
+
+echo "yarn build"
+      yarn build
+
+echo "systemctl restart fukuoka-city"
+      systemctl restart fukuoka-city
